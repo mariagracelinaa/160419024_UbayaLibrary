a32e8b69d403076dc769ca5d04a09c787cfbf540
mariagracelinaa <gracezhouu@gmail.com>
2022-03-18 17:11:32 +0700

    init progress 1 18/3/21

--- app/src/main/java/com/ubaya/a160419024_ubayalibrary/MainActivity.kt
+++ app/src/main/java/com/ubaya/a160419024_ubayalibrary/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ubaya.a160419024_ubayalibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+        bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(drawerLayout) || super.onSupportNavigateUp()
     }
 }
